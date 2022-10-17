e006e20e2b7d16996f6cd871b1d913f2f566e46b
Niels Eppenhof <n.eppenhof@gmail.com>
2022-10-17 20:52:31 +0200

    Added rng utility

--- Engine/src/Core/UUID.cpp
+++ Engine/src/Core/UUID.cpp
@@ -1,14 +1,12 @@
 #include "pch.h"
 #include "UUID.h"
 
+#include "Utility/Random.h"
+
 namespace VoxelEngine
 {
-	static std::random_device s_randomDevice;
-	static std::mt19937_64 s_engine(s_randomDevice());
-	static std::uniform_int_distribution<uint64_t> s_uniformDistribution;
-
 	UUID::UUID()
-		: m_UUID(s_uniformDistribution(s_engine))
+		: m_UUID(Utility::GenerateRandomUInt64())
 	{}
 
 	UUID::UUID(uint64_t uuid)
