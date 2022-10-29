a2f1e8c163a38f2754c5ef9c55d609388e14e717
Niels Eppenhof <n.eppenhof@gmail.com>
2022-10-29 14:29:37 +0200

    Cleanup

--- Engine/src/Renderer/Culling/Frustrum.h
+++ Engine/src/Renderer/Culling/Frustrum.h
@@ -7,17 +7,6 @@
 
 namespace VoxelEngine
 {
-	struct FrustrumPlanes
-	{
-		glm::vec4 NearRight;
-		glm::vec4 NearLeft;
-		glm::vec4 NearTop;
-		glm::vec4 NearBottom;
-
-		glm::vec4 Near;
-		glm::vec4 Far;
-	};
-
 	class Frustrum
 	{
 	public:
