45621c2da0376967ab6ba8ecd2585023a1e5ce44
Niels <n.eppenhof@gmail.com>
2022-12-01 23:25:50 +0100

    Added ability to call C++ methods from C#
    Also changed our test case to be an entity and abstracted some mono stuff.

--- ScriptCore/Source/Main.cs
+++ ScriptCore/Source/Main.cs
@@ -1,32 +1,13 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace VoxelEngine
 {
-    public class Main
+    public static class InternalCalls
     {
-        public Main()
-        {
-            Console.WriteLine("Main constructor!");
-        }
-
-        public void PrintMessage()
-        {
-            Console.WriteLine("Hello world!");
-        }
-
-        public void PrintInt(int value)
-        {
-            Console.WriteLine($"Value is: {value}");
-        }
-
-        public void PrintInts(int value1, int value2)
-        {
-            Console.WriteLine($"Value 1 is: {value1}, value 2 is: {value2}");
-        }
-
-        public void PrintCustomMessage(string message)
-        {
-            Console.WriteLine($"String is: {message}");
-        }
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void NativeLog(string text, int param);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void NativeLog_Vector(ref Vector3 param, out Vector3 result);
     }
 }
